e9c074f1c5640efb9b87bed23ddff2f544a4c648
spriggsc <carynspriggs@gmail.com>
2015-08-24 14:18:49 -0700

    Javascript day 6
    Take home assignment

---
+++ day6.js/moviePlot.js
@@ -0,0 +1,12 @@
+
+
+
+var scripts = new Array();
+scripts[0] = ("Plot     " + ".........." + "Minions Stuart, Kevin and Bob are recruited by Scarlett Overkill, a super-villain who, alongside her inventor husband Herb, hatches a plot to take over the world.");
+scripts[1] = ("Directors       " + ".........." + "  Kyle Balda, Pierre Coffin");
+scripts[2] = ("Writers      " + ".........." + "  Brian Lynch");
+scripts[3] = ("Stars       " + ".........." + "  Sandra Bullock, Jon Hamm, Michael Keaton");
+
+for(i = 0; i < scripts.length; i++){
+	document.write(scripts[i] + "<br >");
+}
